00faf55d337ea18daa809c962fb9dd6b0afbffe4
MaxsForocca <mforocca@unsa.edu.pe>
2025-12-18 23:33:33 -0500

    script camara eje y

--- Assets/Scripts/Camara/CameraFollow2_5D.cs
+++ Assets/Scripts/Camara/CameraFollow2_5D.cs
@@ -9,8 +9,11 @@ public class CameraFollow2_5D : MonoBehaviour
     [SerializeField] private float minX;
     [SerializeField] private float maxX;
 
+    [Header("Limits (Y Axis)")]
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
     [Header("Camera Settings")]
-    [SerializeField] private float fixedY = 0f;
     [SerializeField] private float fixedZ = -10f;
     [SerializeField] private float smoothSpeed = 5f;
 
@@ -19,10 +22,11 @@ public class CameraFollow2_5D : MonoBehaviour
         if (player == null) return;
 
         float targetX = Mathf.Clamp(player.position.x, minX, maxX);
+        float targetY = Mathf.Clamp(player.position.y, minY, maxY);
 
         Vector3 targetPosition = new Vector3(
             targetX,
-            fixedY,
+            targetY,
             fixedZ
         );
 
